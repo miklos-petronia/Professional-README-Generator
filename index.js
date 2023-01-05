464f1753760e9f23c124aff48e47962f768f4652
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-01-05 15:27:39 +0100

    Application

---
+++ index.js
@@ -0,0 +1,7 @@
+// Collection needed for this APP
+const inquirer = require('inquirer');
+const fs = require('fs');
+const createMarkdown = require('./utils/generateMarkdown.js');
+
+// Question for user suggestions; Array
+const questions = []
